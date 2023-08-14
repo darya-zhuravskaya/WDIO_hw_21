97e07a79ec52d0cdf1a041694bea1d290c7b612c
darya-zhuravskaya <darya.zhuravskaya159@gmail.com>
2023-08-14 23:24:23 +0300

    added logs

--- features/step-definitions/steps.ts
+++ features/step-definitions/steps.ts
@@ -12,27 +12,41 @@ Given(/^I am on the home page$/, async () => {
 });
 
 When(/^I click on (.+)$/, async (link) => {
+    logger.info(`Clicking on the link ${link}` )
     await HomePage.clickOn(link)
+    logger.info(`Clicked on the link ${link}` )
 });
 
-Then(/^I should be redirected on the subdomain (.*)$/, async (subodmain) => {
-    expect(await browser.getUrl()).toContain(subodmain);
+Then(/^I should be redirected on the subdomain (.*)$/, async (subdomain) => {
+    logger.info(`Checking redirect to the subdomain ${subdomain}`)
+    expect(await browser.getUrl()).toContain(subdomain);
 });
 
 // Reset Password test
 Given(/^I am on the reset password page$/, async () => {
+    logger.info("Opening reset password page")
     await ResetPasswordPage.open();
+    logger.info("Reset password page is opened")
 });
 
 
 When(/^I enter the phone number (\+\w+)$/, async (phone) => {
+    logger.info(`Entering phone number ${phone}`)
     await ResetPasswordPage.enterPhone(phone);
+    logger.info("Phone number is entered")
+    logger.info("Clicking on submit Button")
     await ResetPasswordPage.submitButton().click();
+    logger.info("Clicked on submit Button")
 });
 
 When(/^I enter an empty string$/, async () => {
+    logger.info("Entering empty string")
     await ResetPasswordPage.enterPhone("");
+    logger.info("Empty string is entered")
+    logger.info("Clicking on submit Button")
     await ResetPasswordPage.submitButton().click();
+    logger.info("Clicked on submit Button")
+    
 });
 
 Then(/^I see the form error message "(.+)"$/, async (message) => {
@@ -41,13 +55,17 @@ Then(/^I see the form error message "(.+)"$/, async (message) => {
 
 // Product Page Test
 Given(/^I am on the product page$/, async () => {
+    logger.info("Opening product page")
     await ProductPage.open();
-    // await browser.pause(1500);
+    logger.info("Product page is opened")
+
 });
 
 When(/^I add the product to the cart$/, async () => {
+    logger.info("Adding product to the cart")
     await ProductPage.addToCart();
     await browser.pause(1500);
+    logger.info("Product is added to the cart")
 });
 
 Then(/^I see the following text on the sidebar "(.+)"$/, async (text) => {
